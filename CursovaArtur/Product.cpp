b21ac81369210425ab83c7e7541165ceea09f1e3
SplyZzZ <dedsv18@gmail.com>
2025-09-24 23:56:48 +0300

    factory mhetod + debug

--- CursovaArtur/Product.cpp
+++ CursovaArtur/Product.cpp
@@ -1,14 +1,33 @@
-#include "Product.h"
+﻿#include "Product.h"
 
 Product::Product() : price(0), size(0), name(" "), description(" ") 
 {
 	ID = ++Unical;
 }
-
 Product::~Product()
 {
 }
-
+void Product::getInformationProduct() const noexcept
+{
+	std::cout << std::left;
+	std::cout << std::setw(14) << "ID:" << ID << std::endl;
+	std::cout << std::setw(14) << "Price:" << price << std::endl;
+	std::cout << std::setw(14) << "Size:" << size << std::endl;
+	std::cout << std::setw(14) << "Name:" << name << std::endl;
+	std::cout << std::setw(14) << "Description:" << description << std::endl;
+}
+void Product::setInformationProduct() noexcept
+{
+	std::cout << "Enter Price: ";
+std::cin >> price;
+std::cout << "Enter Size: ";
+std::cin >> size;
+std::cout << "Enter Name: ";
+std::cin.ignore();
+std::getline(std::cin, name);
+std::cout << "Description: ";
+std::getline(std::cin, description);
+}
 int Product::getID() const noexcept
 {
 	return ID;
@@ -17,46 +36,82 @@ double Product::getPrice() const noexcept
 {
 	return price;
 }
+std::string Product::GetName() noexcept
+{
+	return name;
+}
 int Product::Unical = 0;
 
-Phone::Phone() {}
-
+Phone::Phone() : Product()
+{
+	model = " ";
+}
+Phone::~Phone(){}
 void Phone::getInformationProduct() const noexcept
 {
+	Product::getInformationProduct();
+	std::cin.ignore();
+	std::cout << "Модель: " << model << std::endl;
 }
-
 void Phone::setInformationProduct() noexcept
 {
-	double price;
-	size_t size;
-	std::string name;
-	std::string description;
-std::cout << "Enter Price: ";
-std::cin >> price;
-setPrice(price);
-std::cout << "Enter Size: ";
-std::cin >> size;
-setSize(size);
-std::cout << "Enter Name: ";
-std::cin.ignore();
-std::getline(std::cin, name);
-setName(name);
-std::cout << "Description: ";
-std::getline(std::cin, description);
-setDescription(description);
+	Product::setInformationProduct();
+	std::cout << "Введіть модель телефону: ";
+	std::cin.ignore();
+	std::getline(std::cin, model);
 }
-//std::cout << "Enter Price: ";
-//std::cin >> price;
-//std::cout << "Enter Size: ";
-//std::cin >> size;
-//std::cout << "Enter Name: ";
-//std::cin.ignore();
-//std::getline(std::cin, name);
-//std::cout << "Description: ";
-//std::getline(std::cin, description);
-//std::cout << std::left;
-//std::cout << std::setw(14) << "ID:" << ID << std::endl;
-//std::cout << std::setw(14) << "Price:" << price << std::endl;
-//std::cout << std::setw(14) << "Size:" << size << std::endl;
-//std::cout << std::setw(14) << "Name:" << name << std::endl;
-//std::cout << std::setw(14) << "Description:" << description << std::endl;
+
+TV::TV() : Product()
+{
+	resolution = " ";
+}
+TV::~TV()
+{
+}
+void TV::getInformationProduct() const noexcept
+{
+	Product::getInformationProduct();
+	std::cout << "Розмір: " << resolution << std::endl;
+}
+void TV::setInformationProduct() noexcept
+{
+	Product::setInformationProduct();
+	std::cout << "Введіть розмір: ";
+	std::cin.ignore();
+	std::getline(std::cin, resolution);
+}
+
+exercise::exercise() : Product()
+{
+	type = " ";
+}
+exercise::~exercise(){}
+void exercise::getInformationProduct() const noexcept
+{
+	Product::getInformationProduct();
+	std::cout << "Тип: " << type << std::endl;
+}
+void exercise::setInformationProduct() noexcept
+{
+	Product::setInformationProduct();
+	std::cout << "Введіть тип зарядки: ";
+	std::cin.ignore();
+	std::getline(std::cin, type);
+}
+
+FleshDrive::FleshDrive() : Product()
+{
+	sizeGB = 0;
+}
+FleshDrive::~FleshDrive(){}
+void FleshDrive::getInformationProduct() const noexcept
+{
+	Product::getInformationProduct();
+	std::cout << "GB: " << sizeGB << std::endl;
+}
+void FleshDrive::setInformationProduct() noexcept
+{
+	Product::setInformationProduct();
+	std::cout << "Введіть розмір флешки в гігабайтах: ";
+	std::cin >> sizeGB;
+}
--- CursovaArtur/Product.h
+++ CursovaArtur/Product.h
@@ -2,23 +2,22 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-
+enum class ProductType {
+	Phone,
+	TV,
+	Exercise,
+	FleshDrive
+};
 class Product
 {
 public:
 	Product();
 	virtual ~Product();
-	virtual void getInformationProduct() const noexcept = 0;
-	virtual void setInformationProduct() noexcept = 0;
+	virtual void getInformationProduct() const noexcept;
+	virtual void setInformationProduct() noexcept;
 	int getID() const noexcept;
 	double getPrice() const noexcept;
-	std::string getName() const noexcept;
-	std::string getDescription() const noexcept;
-protected:
-	void setPrice(const double& price)noexcept;
-	void setName(const std::string& name)noexcept;
-	void setDescription(const std::string& description) noexcept;
-	void setSize(const size_t size) noexcept;
+	std::string GetName() noexcept;
 private:
 	int ID;
 	static int Unical;
@@ -26,13 +25,79 @@ private:
 	size_t size;
 	std::string name;
 	std::string description;
-	
 };
 class Phone : public Product
 {
 public:
 	Phone();
-	
+	~Phone();
 	virtual void getInformationProduct() const noexcept override;
 	virtual void setInformationProduct() noexcept override;
+private:
+	std::string model;
+};
+class TV : public Product
+{
+public:
+	TV();
+	~TV();
+	virtual void getInformationProduct() const noexcept override;
+	virtual void setInformationProduct() noexcept override;
+private:
+	std::string resolution;
+};
+class exercise : public Product
+{
+public:
+	exercise();
+	~exercise();
+	virtual void getInformationProduct() const noexcept override;
+	virtual void setInformationProduct() noexcept override;
+private:
+	std::string type;
+};
+class FleshDrive : public Product
+{
+public:
+	FleshDrive();
+	~FleshDrive();
+	virtual void getInformationProduct() const noexcept override;
+	virtual void setInformationProduct() noexcept override;
+private:
+	size_t sizeGB;
+}; 
+class FactoryMethod
+{
+public:
+	static std::shared_ptr<Product> initiProductNew(ProductType type)
+	{
+		switch (type)
+		{
+		case ProductType::Phone:
+		{
+			return std::make_shared<Phone>();
+			break;
+		}
+		case ProductType::TV:
+		{
+			return std::make_shared<TV>();
+			break;
+		}
+		case ProductType::Exercise:
+		{
+			return std::make_shared<exercise>();
+			break;
+		}
+		case ProductType::FleshDrive:
+		{
+			return std::make_shared<FleshDrive>();
+			break;
+		}
+		default:
+		{
+			throw std::invalid_argument("Unknown ProductType");
+			break;
+		}
+		}
+	}
 };
